cf4f348ed99d6c87d1c7f39e682eed7b68da3142
Florian Voelkers <florian.voelkers@mail.de>
2017-08-31 22:54:00 +0200

    HealthManaRegen für alle

--- ARPG/Assets/Scripts/Player/Player.cs
+++ ARPG/Assets/Scripts/Player/Player.cs
@@ -119,9 +119,6 @@ public class Player : NetworkBehaviour {
             InventoryEventHandler.OnItemBagged += AddItemToBag;
             InventoryEventHandler.OnItemUnbagged += RemoveItemFromBag;
             InventoryEventHandler.OnItemDestroyed += DestroyItem;
-
-            InvokeRepeating("RegenManaAndHealth", 1f, 1f);
-
         }
         playerIsOnServer = isServer;
         QuestManager.Instance.isOnServer = playerIsOnServer;
@@ -130,6 +127,7 @@ public class Player : NetworkBehaviour {
             PlayerEventHandler.OnXpGained += GiveXP;
         }
         StartCoroutine(LearnPrimarySkill());
+		InvokeRepeating("RegenManaAndHealth", 1f, 1f);
     }
 
     private void Update()
